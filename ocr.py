1fc8e6ae6e72e7886f79dee720bb696fff00448a
Joe <28671159+JoeYe-233@users.noreply.github.com>
2022-08-15 22:03:30 +0800

    bug fix

--- ocr.py
+++ ocr.py
@@ -6,7 +6,8 @@ import os
 # STEP 2
 sdk = muggle_ocr.SDK(model_type=muggle_ocr.ModelType.OCR)
 
-b=requests.get('https://ua.scu.edu.cn/captcha?captchaId=4057950348')
+a=requests.get('https://ua.scu.edu.cn/captcha?captchaId=4057950348')
+b=a.content
 st = time.time()
 # STEP 3
 text = sdk.predict(image_bytes=b)

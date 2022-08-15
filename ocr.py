fcfbec5bfca0c5c04f69772297d22a13cbe00794
Joe <28671159+JoeYe-233@users.noreply.github.com>
2022-08-15 22:51:09 +0800

    try

--- ocr.py
+++ ocr.py
@@ -5,11 +5,10 @@ import muggle_ocr
 import os
 # STEP 2
 sdk = muggle_ocr.SDK(model_type=muggle_ocr.ModelType.OCR)
-
-a=requests.get('https://ua.scu.edu.cn/captcha?captchaId=4057950348')
-b=a.content
-print(b)
-st = time.time()
-# STEP 3
-text = sdk.predict(image_bytes=b)
-print(text, time.time() - st)
+for i in range(1,10):
+    a=requests.get('https://ua.scu.edu.cn/captcha?captchaId=4057950348')
+    b=a.content
+    st = time.time()
+    # STEP 3
+    text = sdk.predict(image_bytes=b)
+    print("\'"+text+"\':\'"+str(b)+"\',")

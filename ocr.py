5a0f080cd66c1e4e2bcc9365cd7dc22c6c2371e8
Joe <28671159+JoeYe-233@users.noreply.github.com>
2022-08-16 17:31:13 +0800

    1

--- ocr.py
+++ ocr.py
@@ -30,27 +30,65 @@ UA_captcha_url = r'https://ua.scu.edu.cn/captcha?captchaId=4057950348'
 captcha_break_url = r'http://localhost:19952/captcha/v1'
 
 
+
+import time
+import json
+import optparse
+import threading
+import tornado.ioloop
+import tornado.log
+import tensorflow as tf
+from tornado.web import RequestHandler
+from constants import Response, color_map
+from json.decoder import JSONDecodeError
+from tornado.escape import json_decode, json_encode
+from interface import InterfaceManager
+from config import Config
+from utils import ImageUtils, ParamUtils
+from signature import Signature, ServerType
+from watchdog.observers import Observer
+from event_handler import FileEventHandler
+
+
+
+interface_manager = InterfaceManager()
+
+
+def run(input):
+    bytes_batch, response = ImageUtils.get_bytes_batch(input)
+    image_sample = bytes_batch[0]
+    image_size = ImageUtils.size_of_image(image_sample)
+    size_string = "{}x{}".format(image_size[0], image_size[1])
+    interface = interface_manager.get_by_size(size_string)
+
+    split_char = interface.model_conf.split_char
+
+
+
+    image_batch, response = ImageUtils.get_image_batch(interface.model_conf, bytes_batch)
+
+
+    result = interface.predict_batch(image_batch, split_char)
+    response['message'] = result
+    print(json_encode(response))
+    return result
+
+
+
 img_response = requests.get(UA_captcha_url)
 if img_response.status_code == requests.codes['ok']:
-        print(f"成功获取验证码图片\t\t\t\t<httpRespond>[{img_response.status_code}]")
-        # print("成功获取验证码图片\t\t\t", end='')
+    print(f"成功获取验证码图片\t\t\t\t<httpRespond>[{img_response.status_code}]")
+# print("成功获取验证码图片\t\t\t", end='')
 else:
-        print(f"获取图片失败\t\t\t<httpRespond>[{img_response.status_code}]")
+    print(f"获取图片失败\t\t\t<httpRespond>[{img_response.status_code}]")
 
 
 print(f"将图片转发至本地验证码服务器...\t\t{captcha_break_url}")
 # print("将图片转发至本地验证码服务器...\t\t%s" % captcha_break_url)
 break_time = time.time()
 base64EncodedStr = base64.b64encode(img_response.content)
-captcha_to_break = {
-    'image': base64EncodedStr.decode('utf-8')
-}
-captcha_break_response = requests.post(captcha_break_url, json=captcha_to_break)
-captcha_break = json.loads(captcha_break_response.text)
+Result=run(base64EncodedStr.decode('utf-8'))
+
 break_time = time.time() - break_time
-if captcha_break_response.status_code == requests.codes['ok']:
-    print(f"获取预测验证码成功\t预测结果为: {captcha_break['message']} \t[{break_time:.3f}s]")
+print(f"获取预测验证码成功\t预测结果为: {Result} \t[{break_time:.3f}s]")
     # print("获取预测验证码成功\t", end='')
-else:
-    print(f"获取预测失败")
-    # print("获取预测失败\t", end='')
